6ef4c5eba480832a018d1489d12bc93a896ae701
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 <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>
2023-03-20 17:50:14 -0300

    Task 2 first commit

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+def matrix_shape(matrix):
+    shape = []
+    shape.append(len(matrix))
+    while matrix[0] and type(matrix[0]) != int:
+        shape.append(len(matrix[0]))
+        matrix = matrix[0]
+    return (shape)
