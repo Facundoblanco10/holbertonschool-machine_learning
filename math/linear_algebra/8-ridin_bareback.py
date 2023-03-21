12454376b4d74cdafb2976115589b5aa0e80b122
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 <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>
2023-03-21 16:09:52 -0300

    Task 8

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,19 @@
+#!usr/bin/env python3
+"""Perform a matrix multiplication"""
+
+
+def mat_mul(mat1, mat2):
+    """Perform a 2D matrix multiplication"""
+    m1, n1 = len(mat1), len(mat1[0])
+    m2, n2 = len(mat2), len(mat2[0])
+    
+    if n1 != m2:
+        return None
+    
+    result = [[0 for j in range(n2)] for i in range(m1)]
+    
+    for i in range(m1):
+        for j in range(n2):
+            for k in range(n1):
+                result[i][j] += mat1[i][k] * mat2[k][j]
+    return result
