60088e469b5d050cdd9d402b4fa54b569e639e0a
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 <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>
2023-03-20 21:21:12 -0300

    Hot fix

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 """Matrix Transposition"""
 
-
 def matrix_transpose(matrix):
     """Transpose a matrix"""
     transpose = []
     for idx, i in enumerate(matrix):
-        if type(i) == int:
-            transpose = matrix
-            return transpose
         trix = []
+        if len(matrix) == 1:
+            for k in i:
+                trix = []
+                trix.append(k)
+                transpose.append(trix)
+            return (transpose)
         for j in matrix:
             if idx < len(j):
                 trix.append(j[idx])
