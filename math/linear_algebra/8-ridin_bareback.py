a7dfca5640989014c9e0c416c525ad553f255885
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 <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>
2023-03-21 16:10:38 -0300

    Pycodestyle fix

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -6,12 +6,12 @@ def mat_mul(mat1, mat2):
     """Perform a 2D matrix multiplication"""
     m1, n1 = len(mat1), len(mat1[0])
     m2, n2 = len(mat2), len(mat2[0])
-    
+
     if n1 != m2:
         return None
-    
+
     result = [[0 for j in range(n2)] for i in range(m1)]
-    
+
     for i in range(m1):
         for j in range(n2):
             for k in range(n1):
