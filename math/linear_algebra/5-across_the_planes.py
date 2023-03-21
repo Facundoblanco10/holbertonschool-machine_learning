1323485b52b9a6e63eb0fb87fdad5757700c11e9
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 <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>
2023-03-21 11:22:46 -0300

    Task 5 fix

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -8,7 +8,9 @@ def add_matrices2D(mat1, mat2):
             len(mat1[0]) != len(mat2[0]) or
             len(mat1) != len(mat2)):
         return None
+    result = []
     for row1, row2 in zip(mat1, mat2):
-        result = list(map(lambda x, y: x + y, row1, row2))
+        row_result = list(map(lambda x, y: x + y, row1, row2))
+        result.append(row_result)
 
     return result
