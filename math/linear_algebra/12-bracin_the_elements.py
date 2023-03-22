b25323a9e594933a298b40dc17e6315a4d0fe302
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 <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>
2023-03-22 14:53:51 -0300

    Task 12 update

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Element-wise addition, subtraction,"""
+""" division and multiplication"""
+
+
+def np_elementwise(mat1, mat2):
+    """Elementwise function"""
+    add = mat1 + mat2
+
+    sub = mat1 - mat2
+
+    mul = mat1 * mat2
+
+    div = mat1 / mat2
+
+    return (add, sub, mul, div)
+        
