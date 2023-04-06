194ad5e2e6da95844c697578d6bbe2352a0cc00a
Facu B <5078@holbertonstudents.com>
2023-04-05 22:52:36 -0300

    Task 8

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -27,3 +27,9 @@ class Normal():
     def x_value(self, z):
         """Calculates the x-value of a given z-score"""
         return (z * self.stddev) + self.mean
+
+    def pdf(self, x):
+        pi = 3.1415926536
+        e = 2.7182818285
+        return (1 / (self.stddev * ((2 * pi) ** 0.5))) *\
+               (e ** ((self.z_score(x) ** 2) / -2))
