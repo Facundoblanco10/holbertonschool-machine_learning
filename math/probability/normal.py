360d32fc30c432cb2720062d9cf8ffb1edd6a4db
Facu B <5078@holbertonstudents.com>
2023-04-05 22:53:53 -0300

    hot fix

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -29,6 +29,7 @@ class Normal():
         return (z * self.stddev) + self.mean
 
     def pdf(self, x):
+        """Calculates the value of the PDF for a given x-value"""
         pi = 3.1415926536
         e = 2.7182818285
         return (1 / (self.stddev * ((2 * pi) ** 0.5))) *\
