66b9eea874f06b732d1b4589d454032fe197dd36
Facu B <5078@holbertonstudents.com>
2023-04-19 15:53:54 -0300

    updating

---
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Evaluates the output of a neural network"""
+import tensorflow as tf
+
+
+def evaluate(X, Y, save_path):
+    """Evaluates the output of a neural network"""
