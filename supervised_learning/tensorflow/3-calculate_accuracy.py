6fda84a33523362cd9131493827d3b224ada88aa
Facu B <5078@holbertonstudents.com>
2023-04-19 02:26:46 -0300

    Task 3

---
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Forward Propagation"""
+import tensorflow as tf
+
+
+def calculate_accuracy(y, y_pred):
+    """
+    Calculates the accuracy of a prediction.
+    Arguments:
+    y -- placeholder for the labels of the input data
+    y_pred -- tensor containing the network's predictions
+    Returns:
+    tensor containing the decimal accuracy of the prediction
+    """
+    correct_predictions = tf.equal(tf.argmax(y, axis=1),
+                                   tf.argmax(y_pred, axis=1))
+    accuracy = tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
+    return accuracy
