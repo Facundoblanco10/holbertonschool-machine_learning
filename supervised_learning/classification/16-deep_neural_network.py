ae8b8575bdca6678640b4173c255caed8bf795c9
Facu B <5078@holbertonstudents.com>
2023-04-20 16:56:20 -0300

    Task 16 test

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -3,10 +3,12 @@
 Class DeepNeuralNetwork that defines a deep
 neural network performing binary classification
 """
+import numpy as np
 
 
 class DeepNeuralNetwork():
     """Class DeepNeuralNetwork"""
+
     def __init__(self, nx, layers):
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
@@ -14,4 +16,20 @@ class DeepNeuralNetwork():
             raise ValueError("nx must be a positive integer")
         if type(layers) is not list:
             raise TypeError("layers must be a list of positive integers")
-        
+
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+
+        for i in range(self.L):
+            if type(layers[i]) is not int or layers[i] <= 0:
+                raise TypeError("layers must be a list of positive integers")
+
+            if i == 0:
+                w = np.random.randn(layers[i], nx) * np.sqrt(2 / nx)
+            else:
+                w = np.random.randn(
+                    layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
+
+            self.weights[f"b{i+1}"] = np.zeros((layers[i], 1))
+            self.weights[f"W{i+1}"] = w
