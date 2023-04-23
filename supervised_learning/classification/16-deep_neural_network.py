1bd2417bc8332d3db5ad55b9c575e3dd432514b9
Facu B <5078@holbertonstudents.com>
2023-04-22 23:34:44 -0300

    task 16

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -16,20 +16,28 @@ class DeepNeuralNetwork():
             raise ValueError("nx must be a positive integer")
         if type(layers) is not list:
             raise TypeError("layers must be a list of positive integers")
+        if not all(map(lambda item: isinstance(item, int) and
+                       item > 0, layers)):
+            raise ValueError('layers must be a list of positive integers')
 
+        # Initialize number of layers, cache, and weights
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
 
+        # Initialize weights and biases for each layer
         for i in range(self.L):
-            if type(layers[i]) is not int or layers[i] <= 0:
-                raise TypeError("layers must be a list of positive integers")
-
+            # For the first layer, use nx as the number of input features
             if i == 0:
-                w = np.random.randn(layers[i], nx) * np.sqrt(2 / nx)
+                self.weights["W" + str([i + 1])] = \
+                    np.random.randn(layers[i], nx) * np.sqrt(2 / nx)
+
+            # For all other layers, use the number
+            # of neurons in the previous layer
             else:
-                w = np.random.randn(
+                self.weights["W" + str([i + 1])] = \
+                    np.random.randn(
                     layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
 
-            self.weights[f"b{i+1}"] = np.zeros((layers[i], 1))
-            self.weights[f"W{i+1}"] = w
+            # Initialize bias for this layer to be a zero column vector
+            self.weights["b" + str([i + 1])] = np.zeros((layers[i], 1))
