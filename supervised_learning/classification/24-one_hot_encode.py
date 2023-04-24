1897a8431b7645491644aa95735f864406249cf2
Facu B <5078@holbertonstudents.com>
2023-04-23 23:08:09 -0300

    Task 25

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -11,7 +11,11 @@ def one_hot_encode(Y, classes):
             not isinstance(classes, int) or classes <= np.amax(Y):
         return None
 
+    #  initializes a numpy array one_hot with classes rows
+    # and Y.shape[0] columns with all elements set to 0.
     one_hot = np.zeros((classes, Y.shape[0]))
+
+    # sets the value at index [Y[i], i] of one_hot to 1 for all i in Y.
     one_hot[Y, np.arange(Y.shape[0])] = 1
 
     return one_hot
---
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Converts a one-hot matrix into a vector of labels"""
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """
+        Converts a one-hot matrix into a vector of labels
+    """
+    if len(one_hot.shape) != 2 or not isinstance(one_hot, np.ndarray):
+        return None
+    m = one_hot.shape[1]
+
+    # get the index of the maximum value along the axis 0 of the one_hot
+    # array, which returns an array of indices. It then reshapes this
+    # array to be a one-dimensional array of size m using the reshape function
+    return np.argmax(one_hot, axis=0).reshape((m,))
