1e02ed6d60624ddfafe7457375c94efca3b15a4f
Facu Ubuntu16.04 <facundoblancov@gmail.com>
2023-04-30 16:51:55 +0000

    Pycodestyle

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -14,15 +14,14 @@ def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
     @epsilon is a small number to avoid division by zero
     Returns: the Adam optimization operation
     """
-    
     # Create global step variable
     global_step = tf.Variable(0, trainable=False)
-    
+
     # Define Adam optimizer with given hyperparameters
     optimizer = tf.train.AdamOptimizer(learning_rate=alpha, beta1=beta1, beta2=beta2, epsilon=epsilon)
-    
+
     # Compute gradients and apply them using the Adam optimizer
     grads_and_vars = optimizer.compute_gradients(loss)
     train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
-    
+
     return train_op
