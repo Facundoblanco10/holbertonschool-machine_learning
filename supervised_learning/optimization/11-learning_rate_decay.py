2089e6d02695f0501ae516a159722f9b8cc88fa4
Facu Ubuntu16.04 <facundoblancov@gmail.com>
2023-04-30 17:32:25 +0000

    Pycodestyle

--- supervised_learning/optimization/11-learning_rate_decay.py
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -6,9 +6,11 @@ import numpy as np
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """
     @alpha is the original learning rate
-    @decay_rate is the weight used to determine the rate at which alpha will decay
+    @decay_rate is the weight used to
+    determine the rate at which alpha will decay
     @global_step is the number of passes of gradient descent that have elapsed
-    @decay_step is the number of passes of gradient descent that should occur before alpha is decayed further
+    @decay_step is the number of passes of gradient descent
+    that should occur before alpha is decayed further
     Returns: the updated value for alpha
     """
     alpha = alpha / (1 + decay_rate * np.floor(global_step / decay_step))
